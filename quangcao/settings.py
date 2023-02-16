ef5f888789031c5592c2815c640159a8e64c7828
humithdesi <huynhminhthang534@gmail.com>
2023-02-16 22:07:17 +0700

    con me no

--- quangcao/settings.py
+++ quangcao/settings.py
@@ -156,8 +156,10 @@ AWS_QUERYSTRING_AUTH=False
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 filename = uuid.uuid4() 
