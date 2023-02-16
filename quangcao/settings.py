f7948fc897327a566755820f1975ca521f7371a3
humithdesi <huynhminhthang534@gmail.com>
2023-02-16 20:55:46 +0700

    deploy edit

--- quangcao/settings.py
+++ quangcao/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-+w35n6tgcc7f@1fa2fn(i69s+%91h^kv_tno!++8y+-^iu74md'
-SECRET_KEY = os.environ.get('django-insecure-+w35n6tgcc7f@1fa2fn(i69s+%91h^kv_tno!++8y+-^iu74md')
+SECRET_KEY = 'django-insecure-+w35n6tgcc7f@1fa2fn(i69s+%91h^kv_tno!++8y+-^iu74md'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
