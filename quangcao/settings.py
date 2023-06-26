5262b19127b7b87d83d6a7ec6194d25423d571ae
humithdesi <huynhminhthang534@gmail.com>
2023-06-26 23:36:06 +0700

    'capnhat'

--- quangcao/settings.py
+++ quangcao/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'quangcao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'hziXnF7TU8NfxTeq3N5N',
+        'HOST': 'containers-us-west-129.railway.app',
+        'PORT': '6457',
     }
 }
 
